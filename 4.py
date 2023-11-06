5ac1bbd3cf6ce7574021abb618404ba01834fe0e
Sinhan88 <raffaelloligustrowilliam@gmail.com>
2023-11-06 07:27:37 +0700

    day3

--- 4.py
+++ 4.py
@@ -121,3 +121,4 @@ pygame.display.flip()
 time.sleep(1)                                                        # Wait for a few seconds before quitting
 
 pygame.quit()                                                        # Quit pygame
+#
